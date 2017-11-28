0d65e6eaa2f68ea4814bfd50ee5906db4b5fbe49
İbrahim Aşık <asikibrahim93@gmail.com>
2017-11-28 14:18:57 +0300

    api error modified

--- environments/dev/api/config/main-local.php
+++ environments/dev/api/config/main-local.php
@@ -11,7 +11,7 @@ $config = [
 
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
+    /*$config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
     ];
@@ -19,7 +19,7 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-    ];
+    ];*/
 }
 
 return $config;
