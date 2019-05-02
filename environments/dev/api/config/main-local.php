d0cb6a47bea0a3356daff94911b982d4e43a14c4
uguryildiz <yildiz.ugur@gmail.com>
2019-05-02 23:50:54 +0300

    Cookie validation key added to main-local.php

--- environments/dev/api/config/main-local.php
+++ environments/dev/api/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'Ws_5fvKwQV0EaWpFgpgU0x7aK5BsKfPY',
         ],
     ],
 ];
